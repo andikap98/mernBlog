af07baa8c126d94e9e38d45c97695d5760dd78e4
andikap98 <121611042@mhs.uqgresik.ac.id>
2024-02-24 18:43:37 +0700

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
+import mongoose from 'mongoose';
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
+mongoose.connect(process.env.MONGO,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>{
+            console.log('MongooDb is Connected');
+    })
+    .catch((err) =>{
+        console.log(err );
+    })
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!')
 });
